a84db921fdb58d320c89357eb262ace1eb7e2933
endless <123@qq.com>
2026-08-26 10:38:32 +0800

    更新新增员工功能，完成接口前后端联调

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,7 +1,10 @@
 package com.sky.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.sky.constant.MessageConstant;
+import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
@@ -9,9 +12,12 @@ import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
 import com.sky.service.EmployeeService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -54,5 +60,31 @@ public class EmployeeServiceImpl implements EmployeeService {
         //3.2账号正常
         //4.放行
         return employee;
+    }
+
+    /**
+     * 新增员工服务实现
+     * @param employeeDTO
+     */
+    @Override
+    public void save(EmployeeDTO employeeDTO) {
+        //1.将前端传入dto数据封装为entity实体类
+        Employee employee = BeanUtil.copyProperties(employeeDTO, Employee.class);
+        //2.添加未封装的默认数据
+        //2.1设置账号默认状态可用
+        employee.setStatus(StatusConstant.ENABLE);
+        //2.2设置账号创建时间
+        LocalDateTime now = LocalDateTime.now();
+        employee.setCreateTime(now);
+        //2.3设置账号数据更新时间
+        employee.setUpdateTime(now);
+        //2.4设置新增员工/更新员工的操作员
+        //todo 先写死，后期替换
+        employee.setCreateUser(10L);
+        employee.setUpdateUser(10L);
+        //2.5设置默认密码
+        employee.setPassword(PasswordConstant.DEFAULT_PASSWORD);
+        //3.插入employee表中，保存到数据库
+        employeeMapper.insert(employee);
     }
 }
