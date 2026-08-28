994f99706ae2ddb1b9bab59a104143044c38335c
If-n <3043759313@qq.com>
2026-08-28 14:11:28 +0800

    实现菜品批量删除接口功能，

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -6,10 +6,11 @@ import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.enumeration.OperationType;
 import com.sky.vo.DishVO;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+
+import java.util.List;
 
 @Mapper
 public interface DishMapper {
@@ -29,5 +30,32 @@ public interface DishMapper {
     @AutoFill(value = OperationType.INSERT)
     void insert(Dish dish);
 
+    /**
+     * 分页查询菜品
+     * @param dishPageQueryDTO
+     * @return
+     */
     Page<DishVO> queryPage(DishPageQueryDTO dishPageQueryDTO);
+
+    /**
+     * 根据id查询菜品
+     * @param id
+     * @return
+     */
+    @Select("select * from dish where id=#{id}")
+    Dish getById(Long id);
+
+
+    /**
+     * 根据id删除菜品
+     * @param id
+     */
+    @Delete("delete from dish where id=#{id}")
+    void deleteById(Long id);
+
+    /**
+     * 根据id批量删除菜品
+     * @param ids
+     */
+    void deleteByIds(List<Long> ids);
 }
--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -3,12 +3,16 @@ package com.sky.service.impl;
 import cn.hutool.core.bean.BeanUtil;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
+import com.sky.constant.MessageConstant;
+import com.sky.constant.StatusConstant;
 import com.sky.dto.DishDTO;
 import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.entity.DishFlavor;
+import com.sky.exception.DeletionNotAllowedException;
 import com.sky.mapper.DishFlavorMapper;
 import com.sky.mapper.DishMapper;
+import com.sky.mapper.SetmealDishMapper;
 import com.sky.result.PageResult;
 import com.sky.service.DishService;
 import com.sky.vo.DishVO;
@@ -26,6 +30,9 @@ public class DishServiceImpl implements DishService {
 
     @Autowired
     private DishFlavorMapper dishFlavorMapper;
+
+    @Autowired
+    private SetmealDishMapper setmealDishMapper;
 
     /**
      * 新增菜品&口味
@@ -74,4 +81,40 @@ public class DishServiceImpl implements DishService {
         //3.封装并返回数据
         return new PageResult(dishPage.getTotal(),dishPage.getResult());
     }
+
+    /**
+     * 批量删除菜品
+     * @param ids
+     */
+    @Override
+    public void deleteBatch(List<Long> ids) {
+        //1.判断菜品是否能被删除
+        //1.1判断菜品状态，起售则不可删除
+        for (Long id : ids) {
+            //查询每个菜品状态，如果有起售状态则抛异常
+            Dish dish=dishMapper.getById(id);
+            if(dish.getStatus()== StatusConstant.ENABLE){
+                throw new DeletionNotAllowedException(MessageConstant.DISH_ON_SALE);
+            }
+        }
+        //1.2判断每个菜品是否有关联套餐，如果有则不可删除
+        List<Long> mealIds =setmealDishMapper.getSetmealIdsbyDishIds(ids);
+        if(mealIds!=null&&!mealIds.isEmpty()){
+            throw new DeletionNotAllowedException(MessageConstant.DISH_BE_RELATED_BY_SETMEAL);
+        }
+        //可以删除
+        /*for (Long id : ids) {
+            //2.删除dish表中的数据
+            dishMapper.deleteById(id);
+            //3.删除口味表中的数据
+            dishFlavorMapper.deleteByDishId(id);
+        }*/
+
+        //批量删除，减少sql语句
+        dishMapper.deleteByIds(ids);
+        dishFlavorMapper.deleteByDishIds(ids);
+
+
+    }
+
 }
